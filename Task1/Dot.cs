457941636aa3a7eae7be42e2c559621a6c76681d
Uki-coder <pihtatiy.boy@gmail.com>
2023-02-14 09:27:49 +0100

    Task1 (done)

---
+++ Task1/Dot.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Dot
+    {
+        private double XCoordinate;
+        private double YCoordinate;
+        private double ZCoordinate;
+
+        public Dot(double xCoordinate, double yCoordinate, double zCoordinate)
+        {
+            XCoordinate = xCoordinate;
+            YCoordinate = yCoordinate;
+            ZCoordinate = zCoordinate;
+        }
+
+        public Dot()
+        {
+            XCoordinate = 0.0;
+            YCoordinate = 0.0;
+            ZCoordinate = 0.0;
+        }
+
+        public Dot(Dot other)
+        {
+            XCoordinate = other.XCoordinate;
+            YCoordinate = other.YCoordinate;
+            ZCoordinate = other.ZCoordinate;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Dot dot &&
+                   XCoordinate == dot.XCoordinate &&
+                   YCoordinate == dot.YCoordinate &&
+                   ZCoordinate == dot.ZCoordinate;
+        }
+
+        public double Distance(Dot other)
+        {
+            double xDistance = XCoordinate - other.XCoordinate;
+            double yDistance = YCoordinate - other.YCoordinate;
+            double zDistance = ZCoordinate - other.ZCoordinate;
+
+            return Math.Sqrt(
+                (xDistance * xDistance) +
+                (yDistance * yDistance) +
+                (zDistance * zDistance));
+        }
+    }
+}
