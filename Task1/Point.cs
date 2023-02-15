22b106703192c3a0e933517df8cb9f03e40c5ebd
Uki-coder <pihtatiy.boy@gmail.com>
2023-02-15 23:16:35 +0100

    Redone Points
    Redone Dot into Point
    Redone Program with List

--- Task1/Point.cs
+++ Task1/Point.cs
@@ -6,27 +6,27 @@ using System.Threading.Tasks;
 
 namespace Task1
 {
-    internal class Dot
+    internal class Point
     {
         private double XCoordinate;
         private double YCoordinate;
         private double ZCoordinate;
 
-        public Dot(double xCoordinate, double yCoordinate, double zCoordinate)
+        public Point(double xCoordinate, double yCoordinate, double zCoordinate)
         {
             XCoordinate = xCoordinate;
             YCoordinate = yCoordinate;
             ZCoordinate = zCoordinate;
         }
 
-        public Dot()
+        public Point()
         {
             XCoordinate = 0.0;
             YCoordinate = 0.0;
             ZCoordinate = 0.0;
         }
 
-        public Dot(Dot other)
+        public Point(Point other)
         {
             XCoordinate = other.XCoordinate;
             YCoordinate = other.YCoordinate;
@@ -35,13 +35,13 @@ namespace Task1
 
         public override bool Equals(object? obj)
         {
-            return obj is Dot dot &&
+            return obj is Point dot &&
                    XCoordinate == dot.XCoordinate &&
                    YCoordinate == dot.YCoordinate &&
                    ZCoordinate == dot.ZCoordinate;
         }
 
-        public double Distance(Dot other)
+        public double Distance(Point other)
         {
             double xDistance = XCoordinate - other.XCoordinate;
             double yDistance = YCoordinate - other.YCoordinate;
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,27 +1,28 @@
 ﻿using Task1;
 
-Dot[] dots = new Dot[10];
+const int POINTS_AMOUNT = 10;
+List<Point> points = new List<Point>();
 Random rd = new Random();
 
 Console.WriteLine("Dots' coordinates:\n");
 
-for (int i = 0; i < 10; i++)
+for (int i = 0; i < POINTS_AMOUNT; i++)
 {
     double x = (double)rd.Next(-100, 100);
     double y = (double)rd.Next(-100, 100);
     double z = (double)rd.Next(-100, 100);
 
-    dots[i] = new Dot(x, y, z);
-    Console.WriteLine($"Dot {i}: {x}, {y}, {z}");
+    points.Add(new Point(x, y, z));
+    Console.WriteLine($"Point {i}: {x}, {y}, {z}");
 }
 
 Console.WriteLine('\n');
 
-for (int i = 0; i < 9; i++)
+for (int i = 0; i < points.Count() - 1; i++)
 {
-    for (int j = i + 1; j < 10; j++)
+    for (int j = i + 1; j < points.Count(); j++)
     {
-        Console.WriteLine($"Distance between Dot {i} and  Dot {j}: {dots[i].Distance(dots[j])}");
+        Console.WriteLine($"Distance between Point {i} and  Point {j}: {points[i].Distance(points[j])}");
     }
     Console.WriteLine('\n');
 }
